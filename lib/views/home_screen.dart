1cb2ff51fb355df438522c36bf1d78f0bbaa5f62
basit <malikabdulbasit43@gmail.com>
2024-06-11 12:16:42 +0500

    changed the design of peoducts

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -40,24 +40,34 @@ class HomePageState extends State<HomePage> {
           if (productController.products.isEmpty) {
             return const Center(child: CircularProgressIndicator());
           } else {
-            return ListView.builder(
-              itemCount: productController.products.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  child: Column(
-                    children: [
-                      Image(image: NetworkImage(productController.products[index].image!)),
-                      Text(productController.products[index].description!),
-                      Text(productController.products[index].category!),
-                      Text(productController.products[index].price!.toString()),
-                    ],
-                  ),
-                );
-              },
-            );
+            return GridView.builder(
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                itemCount: productController.products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, mainAxisSpacing: 5,crossAxisSpacing: 5,
+                    childAspectRatio: .8
+                ), itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Colors.grey,
+                      child: Column(
+                        children: [
+                          Expanded(child: Image.network(productController.products[index].image!, fit: BoxFit.cover,)),
+                          Text(productController.products[index].price!.toString(), style: TextStyle(fontSize: 18),),
+                        ],
+                      ),
+                    ),
+                  );
+                },);
           }
         },
       ),
+      // Image(image: NetworkImage(productController.products[index].image!)),
+      // Text(productController.products[index].description!),
+      // Text(productController.products[index].category!),
+      // Text(productController.products[index].price!.toString()),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
